d02aa9af3195ececa44dd29e05cd124af83c3de7
fukuharataka <ms121134oknct@gmail.com>
2023-08-03 17:17:25 +0900

    :memo:レコード追加テスト

--- kintone.py
+++ kintone.py
@@ -1,1 +1,34 @@
-print("hello kintone")
+import requests
+import json
+
+# APIトークンを設定
+api_token = "api_token"
+
+# Kintoneのドメイン名
+kintone_domain = "kintone_domain"
+
+# アプリID
+app_id = "0000"
+
+# URLを構築
+url = f"https://{kintone_domain}.kintone.com/k/v1/record.json"
+
+# ヘッダー情報
+headers = {
+    "X-Cybozu-API-Token": api_token,
+    "Content-Type": "application/json"
+}
+
+record ={}
+
+data = {
+    "app": app_id,
+    "record": record
+}
+
+# POSTリクエストを実行
+response = requests.post(url, headers=headers, data=json.dumps(data))
+
+# レスポンスを表示
+print(response.json())
+
